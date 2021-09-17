780bc6b716dd73974e560de72be885b25cab4e23
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-09-17 10:07:23 +0200

    centred modal

--- src/components/ModalMovieInfo/index.jsx
+++ src/components/ModalMovieInfo/index.jsx
@@ -1,18 +1,18 @@
 import { Modal, Button, Card, Row, Col } from 'react-bootstrap'
-// import 'index' from 'ModalMovieInfo/index.css'
+import 'components/ModalMovieInfo/index.css'
 
 const ModalMovieInfo = ({ handleClose, show, movieInfo }) => {
     return (
 
-        <Modal show={show}>
-            <Row>
+        <Modal show={show} centered >
+            <Row class="modal-dialogue">
                 <Col md={6}>
                     {movieInfo && <Card.Img variant="top" src={`${movieInfo.Poster}`} />}
                 </Col>
-                <Col md={6} class="">
+                <Col md={6}>
                     {movieInfo && <h4 class="mt-3">{movieInfo.Title}</h4>}
-                    {movieInfo && <p class="text-justify pe-2">{movieInfo.Plot}</p>}
-                    <Button variant="secondary" class="" onClick={handleClose}>Close</Button>
+                    {movieInfo && <p class="text-justify pe-2 text">{movieInfo.Plot}</p>}
+                    <Button variant="secondary" class="button-close" onClick={handleClose}>Close</Button>
                 </Col>
             </Row>
         </Modal>

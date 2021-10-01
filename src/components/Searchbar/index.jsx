2bff532afce1cbd5ebcb464b307b763bad1a851a
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-10-01 10:12:40 +0200

    add onKeyPress in search form

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -14,7 +14,8 @@ const Searchbar = ({ setInput, fetchMovies }) => {
                 placeholder="Enter a title"
                 className="mr-2"
                 aria-label="Search"
-                onChange={handleInput} />
+                onChange={handleInput}
+                onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} />
             <Button variant="outline-secondary" onClick={fetchMovies} className="ms-2">Search</Button>
         </Form>
     )
